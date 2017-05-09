bf3efcc85616430d9fa5198e2da685887b5ef311
EmersonYe <EmersonYe@gmail.com>
2017-05-09 01:24:09 -0700

    events display succesfully

--- src/HourViewComponent.java
+++ src/HourViewComponent.java
@@ -9,9 +9,10 @@ import javax.swing.event.ChangeListener;
 public class HourViewComponent extends JTextArea implements ChangeListener{
 	LinkedList dataModel;
 	GregorianCalendar displayDay;
-	
+
 	public HourViewComponent(LinkedList dataModel, GregorianCalendar dayToView)
 	{
+		super(0,64);
 		this.dataModel = dataModel;
 		this.displayDay = dayToView;
 		this.setText(formatDay(displayDay));
@@ -19,19 +20,52 @@ public class HourViewComponent extends JTextArea implements ChangeListener{
 	}
 
 
-	
+
 	private String formatDay(GregorianCalendar displayDay) {
 		String formattedString = "";
-		
-		for (int i = 1; i < 25; i++)
+
+		for (int i = 0; i < 24; i++)
 		{
-			formattedString += i + ":00\n";
+			formattedString += i + ":00";
+			GregorianCalendar hour = dataModel.getDayToView();
+			hour.set(Calendar.HOUR_OF_DAY, i);
+			Event eventChecker = eventAtTime(hour);
+			//there is an event
+			if(eventChecker != null)
+			{
+				formattedString += " "+eventChecker.getTitle();
+			}
+
+			formattedString += "\n";
 		}
-		
+
 		return formattedString;
 	}
 
+	private Event eventAtTime(GregorianCalendar c)
+	{
+		LinkedList.Pointer pointer = dataModel.listPointer();
+		while(pointer.hasNext())
+		{
+			Event currentEvent = (Event)pointer.get();
+			if( currentEvent.getStart().get(Calendar.YEAR) == c.get(Calendar.YEAR) &&
+					currentEvent.getStart().get(Calendar.MONTH) == c.get(Calendar.MONTH) && 
+					currentEvent.getStart().get(Calendar.DAY_OF_MONTH) == c.get(Calendar.DAY_OF_MONTH) &&
+					currentEvent.getStart().get(Calendar.HOUR_OF_DAY) == c.get(Calendar.HOUR_OF_DAY))
+			{
+				//event found
+				return currentEvent;
+			}
+			else
+			{
+				//System.out.println("event found on month" + currentEvent.getStart().get(Calendar.MONTH));
+			}
+			if(pointer.hasNext())
+				pointer.next();
 
+		}
+		return null;
+	}
 
 	@Override
 	public void stateChanged(ChangeEvent e) {
